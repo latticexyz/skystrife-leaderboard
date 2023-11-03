1f24f22e2bfc377e8479ef244975693a71626175
Fraser Scott <fraserdscott@gmail.com>
2023-11-03 15:57:36 +0000

    chore: delete unused imports

--- packages/contracts/src/systems/PilferSystem.sol
+++ packages/contracts/src/systems/PilferSystem.sol
@@ -1,26 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.21;
 
-import { Bytes } from "@latticexyz/store/src/Bytes.sol";
 import { System } from "@latticexyz/world/src/System.sol";
-import { PackedCounter } from "@latticexyz/store/src/PackedCounter.sol";
 
 import { Combat, Position, PositionData } from "contracts-skystrife/src/codegen/index.sol";
 
 import { Balances, Position as ScavengerPosition, PositionData as ScavengerPositionData, Pilfered } from "../codegen/index.sol";
-import { Direction } from "../codegen/common.sol";
 
 uint256 constant REWARD = 100;
 
 contract PilferSystem is System {
- function pilfer(bytes32 matchEntity, bytes32 entity) public {
-  require(Combat.getHealth(matchEntity, entity) > 0, "Entity is not a unit");
-  require(!Pilfered.get(matchEntity, entity), "Entity has already been pilfered");
-  ScavengerPositionData memory position = ScavengerPosition.get(matchEntity, _msgSender());
-  PositionData memory targetPosition = Position.get(matchEntity, entity);
-  require(position.x == targetPosition.x && position.y == targetPosition.y, "You must be on same tile");
+  function pilfer(bytes32 matchEntity, bytes32 entity) public {
+    require(Combat.getHealth(matchEntity, entity) > 0, "Entity is not a unit");
+    require(!Pilfered.get(matchEntity, entity), "Entity has already been pilfered");
 
-  Pilfered.set(matchEntity, entity, true);
-  Balances.set(_msgSender(), Balances.get(_msgSender()) + REWARD);
- }
+    ScavengerPositionData memory position = ScavengerPosition.get(matchEntity, _msgSender());
+    PositionData memory targetPosition = Position.get(matchEntity, entity);
+    require(position.x == targetPosition.x && position.y == targetPosition.y, "You must be on same tile");
+
+    Pilfered.set(matchEntity, entity, true);
+    Balances.set(_msgSender(), Balances.get(_msgSender()) + REWARD);
+  }
 }
